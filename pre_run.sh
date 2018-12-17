823577338b8bdea2e75e13bfca380e44ad23bf8c
skyfire <skyfireitdiy@hotmail.com>
2018-12-17 15:48:50 +0800

    恢复

--- pre_run.sh
+++ pre_run.sh
@@ -6,6 +6,5 @@ if [ `id -u` -ne 0 ]; then
 fi
 
 set -e
-docker pull base/archlinux --registry-mirror=https://docker.mirrors.ustc.edu.cn
 docker build -t feather_demo .
-docker pull mysql --registry-mirror=https://docker.mirrors.ustc.edu.cn
+docker pull mysql
