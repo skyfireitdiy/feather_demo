0af3794bc95ef0a8a596d81f53bd2ca495bbdaa5
王茂斌 <skyfireitdiy@hotmail.com>
2018-12-17 15:41:35 +0800

    Update pre_run.sh

--- pre_run.sh
+++ pre_run.sh
@@ -6,5 +6,6 @@ if [ `id -u` -ne 0 ]; then
 fi
 
 set -e
+docker pull base/archlinux --registry-mirror=https://docker.mirrors.ustc.edu.cn
 docker build -t feather_demo .
-docker pull mysql
+docker pull mysql --registry-mirror=https://docker.mirrors.ustc.edu.cn
